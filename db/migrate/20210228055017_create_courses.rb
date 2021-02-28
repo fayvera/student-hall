6b53075c0eb57824d16450ba964adfb1e4e280f5
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-28 03:02:16 -0500

    adds database file and completes migration:

--- db/migrate/20210228055017_create_courses.rb
+++ db/migrate/20210228055017_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :courses do |t|
       t.string :name
-      t.string :description 
+      t.text :description 
     end
   end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Course.create(:name => "History", :description => "This class explores the events of the past in an attempt to understand them.")
+Course.create(:name => "Harry Potter Studies", :description => "This course dives into the world of witchcraft and wizardry that we know and love. The Harry Potter Studies covers a number of topics which are presented in the series.")
+Course.create(:name => "Anatomy", :description => "This course's focus is the study of the human body and its structure.")
+Course.create(:name => "Portuguese", :description => "The study of the Portuguese language, its grammar, and daily usage.")
+# Student.create(:first_name => "Rose", :last_name => "Hathaway", :email => "roseh@gmail.com")
+
